0117044988a253ca8200bc9a5754fbe2c9ec6c19
unknown <datapointprojects13@gmail.com>
2021-08-23 15:39:15 +0530

    Modified user Functionality

--- blog/main.py
+++ blog/main.py
@@ -4,6 +4,7 @@ from database import engine,SessionLocal
 import schemas, models
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from typing import List
 from hashing import Hash
 
 
@@ -26,12 +27,12 @@ def create(request:Blog, db:Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog',response_model=List[schemas.ShowBlog])
 def all(db:Session=Depends((get_db))):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}',status_code=200)
+@app.get('/blog/{id}',status_code=200, response_model=schemas.ShowBlog)
 def show(id,response:Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -42,7 +43,10 @@ def show(id,response:Response, db:Session=Depends(get_db)):
 
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id,db:Session=Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'Done'
 
@@ -61,8 +65,8 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @app.post('/user')
 def create_user(request: schemas.User, db:Session=Depends(get_db)):
     hashedPassword = pwd_cxt.hash(request.password)
-    #new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
-    new_user = models.User(name=request.name, email=request.email, password=hashedPassword)
+    new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
+    #new_user = models.User(name=request.name, email=request.email, password=hashedPassword)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
