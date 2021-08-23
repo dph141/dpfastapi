b71222641c982b6a6761c37dc8cc8d8b670a8d89
unknown <datapointprojects13@gmail.com>
2021-08-23 16:41:31 +0530

    Relationship Created

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,30 @@
 from pydantic import BaseModel
-class Blog(BaseModel):
+from typing import List
+
+class BlogBase(BaseModel):
     title:str
     body:str
 
-class ShowBlog(BaseModel):
-    title:str
-    body:str
+class Blog(BaseModel):
     class Config():
-        orm_mode = True
+        orm_mode=True
+
+
 class User(BaseModel):
     name:str
     email:str
     password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    blogs:List[Blog]=[]
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    creator:ShowUser
+    class Config():
+        orm_mode = True
